66280c8b3de05152f0b4846de2696edd834a0f15
YMXD <1275940484@qq.com>
2020-11-27 09:48:18 +0800

    第一次提交：初始化

---
+++ src/main/java/com/edu/guraduration/GuradurationApplication.java
@@ -0,0 +1,13 @@
+package com.edu.guraduration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuradurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuradurationApplication.class, args);
+    }
+
+}
